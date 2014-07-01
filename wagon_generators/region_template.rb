44778bf7bfb3a57f68b7b502a962a6ee3441f817
Jiří Kubíček <jiri@kubicek.cz>
2014-07-02 00:38:23 +0200

    [REGION] organization_id

--- wagon_generators/region_template.rb
+++ wagon_generators/region_template.rb
@@ -2,6 +2,12 @@ class RegionTemplate < Locomotive::Wagon::Generators::Site::Base
   def self.source_root
     File.join(File.dirname(__FILE__), 'region_template')
   end
+
+  def copy_sources
+    @organization_id = ask('ID organizace v registru Svobodných?')
+    super
+  end
+
 end
 
 Locomotive::Wagon::Generators::Site.register(:region_template, RegionTemplate, %{

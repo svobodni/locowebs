34671747d446b376eadf0e7170e76694cace4ec9
Jiří Kubíček <jiri@kubicek.cz>
2014-07-10 01:59:25 +0200

    [PRAHA] Generated web

--- wagon_generators/region_template.rb
+++ wagon_generators/region_template.rb
@@ -6,7 +6,7 @@ class RegionTemplate < Locomotive::Wagon::Generators::Site::Base
   def copy_sources
     @region_id = ask('ID kraje v registru Svobodných?')
     super
-    File.symlink(File.join(File.dirname(__FILE__), '../config/deploy.yml'), File.join(destination,'config/deploy.yml'))
+    File.symlink('../config/deploy.yml', File.join(destination,'config/deploy.yml'))
   end
 
 end
